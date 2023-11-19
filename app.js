84ad811582b01882a1736fbbdd65d023855c3311
ashutoshdas18@gmail.com <ashutoshdas18@gmail.com>
2023-11-19 22:39:27 +0530

    experiment

--- app.js
+++ app.js
@@ -14,38 +14,38 @@ app.use(cors());
 app.use(express.json());
 
 // Endpoint to handle incoming requests
-app.post('/request', async (req, res) => {
-  // Assuming the request body contains a field named "string"
-  const receivedString = req.body.string;
+// app.post('/request', async (req, res) => {
+//   // Assuming the request body contains a field named "string"
+//   const receivedString = req.body.string;
 
-  // Save the received string to the variable 'url'
-  // You can replace 'url' with any variable name you prefer
-  // Do something with the string, for now, we are logging it
-  console.log('Received String:', receivedString);
+//   // Save the received string to the variable 'url'
+//   // You can replace 'url' with any variable name you prefer
+//   // Do something with the string, for now, we are logging it
+//   console.log('Received String:', receivedString);
 
-  // Axios configuration with the dynamic URL
-  let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: receivedString,
-    headers: {
-      'Api-Key': 'Dt49ZXVqqDVspIRChYULD5hTOo44vpeJ',
-      'User-Agent':'PostmanRuntime/7.35.0'
-    },
-  };
+//   // Axios configuration with the dynamic URL
+//   let config = {
+//     method: 'get',
+//     maxBodyLength: Infinity,
+//     url: receivedString,
+//     headers: {
+//       'Api-Key': 'Dt49ZXVqqDVspIRChYULD5hTOo44vpeJ',
+//       'User-Agent':'PostmanRuntime/7.35.0'
+//     },
+//   };
 
-  try {
-    // Make the Axios GET request
-    const axiosResponse = await axios(config);
+//   try {
+//     // Make the Axios GET request
+//     const axiosResponse = await axios(config);
 
-    // Return the result from the GET request
-    res.status(200).json(axiosResponse.data);
-  } catch (error) {
-    // Handle errors
-    console.error('Axios GET request error:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+//     // Return the result from the GET request
+//     res.status(200).json(axiosResponse.data);
+//   } catch (error) {
+//     // Handle errors
+//     console.error('Axios GET request error:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
 app.post('/download', async (req, res) => {
   try {
